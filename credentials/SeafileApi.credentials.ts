8ddff5accad75e7167e7ccf33932547acb8f3821
Vitali Quiering <vitali@quiering.com>
2023-10-20 21:49:08 +0200

    Update Seafile API credentials and node functionality
    - Added 'Instance URL' field to Seafile API credentials
    - Removed Seafile.node.json file
    - Expanded Seafile.node.ts to include 'Download' operation
    - Added error handling for missing credentials in Seafile.node.ts
    - Updated request options for 'Create' and 'Download' operations in Seafile.node.ts

--- credentials/SeafileApi.credentials.ts
+++ credentials/SeafileApi.credentials.ts
@@ -1,15 +1,21 @@
 import { ICredentialType, NodePropertyTypes } from 'n8n-workflow';
 
 export class SeafileApi implements ICredentialType {
-	name = 'seafileApi';
-	displayName = 'Seafile API';
-	documentationUrl = 'seafile';
-	properties = [
-		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string' as NodePropertyTypes,
-			default: '',
-		},
-	];
+    name = 'seafileApi';
+    displayName = 'Seafile API';
+    documentationUrl = 'seafile';
+    properties = [
+        {
+            displayName: 'API Key',
+            name: 'apiKey',
+            type: 'string' as NodePropertyTypes,
+            default: '',
+        },
+        {
+            displayName: 'Instance URL',
+            name: 'url',
+            type: 'string' as NodePropertyTypes,
+            default: '',
+        },
+    ];
 }
--- nodes/seafile/Seafile.node.ts
+++ nodes/seafile/Seafile.node.ts
@@ -1,63 +1,122 @@
-import { IExecuteFunctions } from 'n8n-core';
-import { INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
+import {
+    IExecuteFunctions
+} from 'n8n-core';
+import {
+    INodeExecutionData,
+    INodeType,
+    INodeTypeDescription,
+    NodeOperationError
+} from 'n8n-workflow';
 
 export class Seafile implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Seafile',
-		name: 'seafile',
-		icon: 'file:seafile.png',
-		group: ['input'],
-		version: 1,
-		description: 'Consume Seafile API',
-		defaults: {
-			name: 'Seafile',
-			color: '#1A82e2',
-		},
-		inputs: ['main'],
-		outputs: ['main'],
-		credentials: [
-			{
-				name: 'seafileApi',
-				required: true,
-			},
-		],
-		properties: [
-			{
-				displayName: 'Operation',
-				name: 'operation',
-				type: 'options',
-				options: [
-					 {
-							name: 'Create a file or directory',
-							value: 'create',
-							description: 'Create a new file or directory',
-					 },
-					 // Other operations here
-				],
-				default: 'create',
-				description: 'The operation to perform.'
-		 },
-		],
-	};
+    description: INodeTypeDescription = {
+        displayName: 'Seafile',
+        name: 'seafile',
+        icon: 'file:seafile.png',
+        group: ['input'],
+        version: 1,
+        description: 'Consume Seafile API',
+        defaults: {
+            name: 'Seafile',
+            color: '#1A82e2',
+        },
+        inputs: ['main'],
+        outputs: ['main'],
+        credentials: [
+            {
+                name: 'seafileApi',
+                required: true,
+            },
+        ],
+        properties: [
+            {
+                displayName: 'Operation',
+                name: 'operation',
+                type: 'options',
+                options: [
+                    {
+                        name: 'Create',
+                        value: 'create',
+                        description: 'Create a file',
+                    },
+                    {
+                        name: 'Download',
+                        value: 'download',
+                        description: 'Download a file',
+                    },
+                ],
+                default: 'create',
+                description: 'The operation to perform.',
+            },
+            {
+                displayName: 'Repository ID',
+                name: 'repo_id',
+                type: 'string',
+                default: '',
+                required: true,
+                description: 'The ID of the repository.',
+            },
+            {
+                displayName: 'Path',
+                name: 'path',
+                type: 'string',
+                default: '/',
+                required: true,
+                description: 'Path to the directory.',
+            },
+            {
+                displayName: 'Filename',
+                name: 'filename',
+                type: 'string',
+                default: '',
+                required: true,
+                description: "The file's name.",
+            },
+            {
+                displayName: 'Content',
+                name: 'content',
+                type: 'string',
+                default: '',
+                displayOptions: { show: { operation: ['create'] } },
+                description: "The file's content.",
+            },
+        ],
+    };
 
-	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		const items = this.getInputData();
-		const returnData = [];
-		const operation = this.getNodeParameter('operation', 0);
+    async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+        const items = this.getInputData();
+        const operation = this.getNodeParameter('operation', 0);
+        const repo_id = this.getNodeParameter('repo_id', 0);
+        const path = this.getNodeParameter('path', 0);
+        const filename = this.getNodeParameter('filename', 0);
 
-		for (let i = 0; i < items.length; i++) {
-			 if(operation === 'create') {
-			// Your logic to connect to the Seafile API goes here.
-			// You can use this.getWorkflowStaticData('node') to get the static data
-			// You can use this.getCredentials('seafileApi') to get the credentials
-			// You can use this.helpers.request() to make API requests
-		}
+        const credentials = this.getCredentials('seafileApi');
+        if (!credentials) {
+            throw new NodeOperationError(this.getNode(), 'No credentials got returned!');
+        }
 
-		for (let i = 0; i < returnData.length; i++) {
-			responseData = { ...returnData[i] };
-			this.addItem(returnData, responseData);
-		}
+        let options = {};
 
-		return this.prepareOutputData(returnData);
-	}
+        if (operation === 'create') {
+            const content = this.getNodeParameter('content', 0);
+            options = {
+                method: 'POST',
+                uri: `${credentials.url}/api2/repos/${repo_id}/file/?p=${path}`,
+                formData: {
+                    filename: filename,
+                    content: content,
+                },
+            };
+        } else if (operation === 'download') {
+            options = {
+                method: 'GET',
+                uri: `${credentials.url}/api2/repos/${repo_id}/file/?p=${path}${filename}`,
+            };
+        }
+
+        const response = await this.helpers.request(options);
+
+        return [this.helpers.returnJsonArray([response])];
+
+    }
 }
