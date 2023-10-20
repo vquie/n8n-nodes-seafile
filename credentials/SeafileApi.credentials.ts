6c68596d2d518f2e8d8023647c6b87c22885bb10
Vitali Quiering <vitali@quiering.com>
2023-10-20 21:02:44 +0200

    rework

--- credentials/SeafileApi.credentials.ts
+++ credentials/SeafileApi.credentials.ts
@@ -1,57 +1,15 @@
-import type {
-	ICredentialDataDecryptedObject,
-	ICredentialTestRequest,
-	ICredentialType,
-	IHttpRequestOptions,
-	INodeProperties,
-} from 'n8n-workflow';
+import { ICredentialType, NodePropertyTypes } from 'n8n-workflow';
 
 export class SeafileApi implements ICredentialType {
 	name = 'seafileApi';
-
 	displayName = 'Seafile API';
-
 	documentationUrl = 'seafile';
-
-	properties: INodeProperties[] = [
+	properties = [
 		{
-			displayName: 'Web DAV URL',
-			name: 'webDavUrl',
-			type: 'string',
-			placeholder: 'https://seafile.example.com/seafdav',
-			default: '',
-		},
-		{
-			displayName: 'User',
-			name: 'user',
-			type: 'string',
-			default: '',
-		},
-		{
-			displayName: 'Password',
-			name: 'password',
-			type: 'string',
-			typeOptions: { password: true },
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
 			default: '',
 		},
 	];
-
-	async authenticate(
-		credentials: ICredentialDataDecryptedObject,
-		requestOptions: IHttpRequestOptions,
-	): Promise<IHttpRequestOptions> {
-		requestOptions.auth = {
-			username: credentials.user as string,
-			password: credentials.password as string,
-		};
-		return requestOptions;
-	}
-
-	test: ICredentialTestRequest = {
-		request: {
-			baseURL: "={{$credentials.webDavUrl.replace('/seafdav', '')}}",
-			url: '/ocs/v1.php/cloud/capabilities',
-			headers: { 'OCS-APIRequest': true },
-		},
-	};
 }
